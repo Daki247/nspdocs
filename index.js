4334923b0db0d365d5f86a8082e8004d4b59c44b
Daki247 <157619531+Daki247@users.noreply.github.com>
2025-01-23 14:14:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function callnwpage()
+{
+    window.location='https://www.google.com/';
+}
+function facebook()
+{
+    window.location='https://www.facebook.com/ch.chowdary.12';
+}
+function instagram()
+{
+    window.location='https://www.instagram.com/nityastotra.parayanam';
+}
+function whatsapp()
+{
+    window.location='https://wa.me/message/NPP6Z2GD5TEGO1';
+}
