0bb943084356854bc33346732c9e7db563e24179
Daki247 <157619531+Daki247@users.noreply.github.com>
2025-01-23 15:13:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function callnwpage()
 {
-    window.location='https://l.facebook.com/l.php?u=https%3A%2F%2Fforms.gle%2Fi1kd2erpCtb7HSUS6%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR3MpWYW5nnoZtOvPo3em_GHkRdWcvZE5A-YKEY52XSai4zdesp6dfdCMw8_aem_EPtCTdkbAumuNB6JmUgO5A&h=AT2aE1dhFMdawgGyy2XwSu6V4AcVLP5FQtnctPWwq0WsCj9X9_LaKyAMphRcHyUMWs0SreNRZoTNuRgIYMdAGi7LhgutEN0tw15vn4V8OOX83jndXTpcBJ4JdgXqNhZASPkwq5CGjpRnGZtaCSgA-Q';
+    window.open('https://l.facebook.com/l.php?u=https%3A%2F%2Fforms.gle%2Fi1kd2erpCtb7HSUS6%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR3MpWYW5nnoZtOvPo3em_GHkRdWcvZE5A-YKEY52XSai4zdesp6dfdCMw8_aem_EPtCTdkbAumuNB6JmUgO5A&h=AT2aE1dhFMdawgGyy2XwSu6V4AcVLP5FQtnctPWwq0WsCj9X9_LaKyAMphRcHyUMWs0SreNRZoTNuRgIYMdAGi7LhgutEN0tw15vn4V8OOX83jndXTpcBJ4JdgXqNhZASPkwq5CGjpRnGZtaCSgA-Q', '_blank');
 }
 function facebook()
 {
-    window.location='https://www.facebook.com/profile.php?id=61567852370903';
+    window.open('https://www.facebook.com/profile.php?id=61567852370903', '_blank');
 }
 function instagram()
 {
-    window.location='https://www.instagram.com/nityastotra.parayanam';
+    window.open('https://www.instagram.com/nityastotra.parayanam', '_blank');
 }
 function whatsapp()
 {
-    window.location='https://wa.me/+918050935734';
+    window.open('https://wa.me/+918050935734', '_blank');
 }
